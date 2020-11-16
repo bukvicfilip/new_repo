043224271712e1a66911d9a43fc10b447515a7b3
Filip Bukvić <bukvifilip6@gmail.com>
2020-11-16 19:14:27 +0100

    First Commit

---
+++ udemy4.py
@@ -0,0 +1,10 @@
+def mode(list_of_nums):
+	dict_of_nums={}
+	for num in list_of_nums:
+		dict_of_nums[num]=list_of_nums.count(num)
+	m=max(dict_of_nums.values())
+	list_of_values=([value for value in dict_of_nums.values()]).index(m)
+	print(list(dict_of_nums.keys())[list_of_values])
+
+
+mode([2,4,1,2,3,3,4,4,5,4,4,6,4,6,7,4, 8]) # 4
